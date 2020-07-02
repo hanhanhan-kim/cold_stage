8cbacf095f84a387216c3aa3e2967b8a44253799
Han Kim <hankim@caltech.edu>
2020-07-02 00:28:39 -0700

    init script for making pwm temp timeseries

---
+++ data/feedforward_calibration/make_PWM_temp_timeseries.py
@@ -0,0 +1,23 @@
+import glob 
+import datetime
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+csvs = sorted(glob.glob("*.csv"))
+dfs = [pd.read_csv(csv, names=["time", "temperature (C)"]) for csv in csvs]
+
+for df, csv in zip(dfs,csvs):
+    
+    # Convert time string to datetime object:
+    df["time"] =  pd.to_datetime(df["time"])
+    
+    # Plot:
+    plt.figure(num=None, figsize=(12, 4), dpi=100, facecolor='w', edgecolor='k')
+    plt.plot(df["time"], df["temperature (C)"])
+    plt.xlabel("time")
+    plt.ylabel("temperature (C)")
+    plt.grid(True)
+    plt.show()
